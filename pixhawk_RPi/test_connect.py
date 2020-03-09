9e55e8dd09eded5a09e64c6dc4f6bd7b4e04583a
Jaehyeok Kim <jeffersonkim97@gmail.com>
2020-03-08 22:18:42 -0400

    Updated script

--- pixhawk_RPi/test_connect.py
+++ pixhawk_RPi/test_connect.py
@@ -1,15 +1,15 @@
 # Imports
-import dronekit_sitl
+import time
 from dronekit import connect, VehicleMode
 
 # Setup
-sitl = dronekit_sitl.start_default()
-connection_string = sitl.connection_string()
+connection_string = "/dev/ttyACM0"
+baud_rate = 115200
 
 # Establishing vehicle connection
 print("start connection...")
 print("Connecting to vehicle on: %s" % (connection_string,))
-vehicle = connect(connection_string, wait_ready=True)
+vehicle = connect(connection_string, baud = baud_rate, wait_ready=True)
 
 # Return vehicle attributes (State)
 print("Vehicle attribute values:")
@@ -18,9 +18,18 @@ print("  Last Heartbeat: %s" % vehicle.last_heartbeat)
 print("  System status: %s" % vehicle.system_status.state)
 print("  Mode: %s" % vehicle.mode.name)
 
+def attitude_callback(self, attr_name, value):
+    print(vehicle.attitude)
+
+print("")
+print("Adding an attitude listener")
+vehicle.add_attribute_listener('attitude', attitude_callback)
+time.sleep(5)
+
+vehicle.remove_attribute_listener('attitude', attitude_callback)
+
 # Close vehicle object
 vehicle.close()
 
 # End sitl
-sitl.stop()
 print("Completed")
