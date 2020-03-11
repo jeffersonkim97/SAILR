872f95d74b943ceee39f671ab9df0fa4797161f4
Jaehyeok Kim <jeffersonkim97@gmail.com>
2020-03-10 20:17:04 -0400

    Pixhawk/RPi connection setup Need to obtain GPS coordinate and write to file

--- pixhawk_RPi/test_connect.py
+++ pixhawk_RPi/test_connect.py
@@ -3,30 +3,53 @@ from dronekit import connect, Command, LocationGlobal
 from pymavlink import mavutil
 import time, sys, argparse, math
 
+# Python Function
+def attitude_callback(self, attr_name, value):
+    """
+    Return current pixhawk attitude
+    @param attr_name: name of attitude (pitch, roll, yaw)
+    @param value: attitude value
+    """
+    print(vehicle.attitude)
+
 # Setup
-connection_string = "/dev/ttyACM0"
-baud_rate = 115200
+connection_string = "/dev/ttyS0"
+baud_rate = 921600
 
 # Establishing vehicle connection
 print("Start connection...")
 print("Connecting to vehicle on: %s" % (connection_string,))
-vehicle = connect(connection_string, wait_ready=False) #baud = baud_rate, 
+vehicle = connect(connection_string, baud = baud_rate, wait_ready=False) #
 vehicle.wait_ready(True, timeout=300)
 print("Vehicle connected!")
+comm_active = True
 
-# Return vehicle attributes (State)
-print("Vehicle attribute values:")
-print("  GPS: %s" % vehicle.gps_0)
-print("  Last Heartbeat: %s" % vehicle.last_heartbeat)
-print("  System status: %s" % vehicle.system_status.state)
-print("  Mode: %s" % vehicle.mode.name)
+#while comm_active:
+    # Return vehicle attributes (State)
+    #print("")
+    #print("Adding an attitude listener")
+    #vehicle.add_attribute_listener('attitude', attitude_callback)
 
-def attitude_callback(self, attr_name, value):
-    print(vehicle.attitude)
 
-print("")
-print("Adding an attitude listener")
-vehicle.add_attribute_listener('attitude', attitude_callback)
+print(vehicle.gps_0)
+print(vehicle.mode)
+
+start = time.time()
+elapsed_time = 0
+while comm_active:
+    print("===================")
+    print("At time %s" %elapsed_time)
+    print("%s" %vehicle.attitude)
+    print("%s" %vehicle.location.global_frame)
+    time.sleep(1)
+    end = time.time()
+    elapsed_time = end - start
+    comm_active = False
+
+
+#print("%s" %vehicle.location.global_frame)
+#print("%s" %vehicle.location.global_relative_frame)
+
 time.sleep(5)
 
 vehicle.remove_attribute_listener('attitude', attitude_callback)
