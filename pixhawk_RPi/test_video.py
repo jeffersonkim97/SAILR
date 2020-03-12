38951587408c46c05bc3058ab95b9485bafcd925
Jaehyeok Kim <jeffersonkim97@gmail.com>
2020-03-12 19:33:24 -0400

    Video recording program

---
+++ pixhawk_RPi/test_video.py
@@ -0,0 +1,23 @@
+# import
+from picamera.array import PiRGBArray
+from picamera import PiCamera
+import time
+import cv2
+
+# initialize the camera
+camera = PiCamera()
+camera.resolution(640, 480)
+camera.framerate = 32
+rawCapture = PiRGBArray(camera, size=(640, 480))
+
+# camera warmup
+time.sleep(1)
+
+# Start capture
+for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
+    image = frame.array
+    cv2.imshow("Frame", image)
+    key = cv2.waitKey(1) & 0xFF
+    rawCapture.truncate(0)
+    if key == ord("q"):
+        break
